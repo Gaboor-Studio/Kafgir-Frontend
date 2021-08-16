41dda9c79ea9b08727a466a926adf179e728845d
KianoshArian <kianosharian3@gmail.com>
2021-08-16 19:59:58 +0430

    added week selector visuals

--- src/Components/Pages/Planner/Planner.js
+++ src/Components/Pages/Planner/Planner.js
@@ -1,8 +1,33 @@
 import React from "react";
+import classes from "./Planner.module.css"
+import leftArrow from "../../../assets/left-arrow.png"
+import rightArrow from "../../../assets/right-arrow.png"
 const planner = (props) => {
+    const weeks=[
+        {
+            period: "۲۶ تیر - ۱ مرداد",
+
+        },
+        {
+            period: "۲ تیر تا ۸ تیر",
+
+        },
+        {
+            period: "۹ تیر تا ۱۵ تیرس",
+
+        },
+        {
+            period: "۱۶ تیر تا ۲۲ تیر",
+
+        }
+    ]
     return (
         <div>
-            Planner
+            <div dir="rtl" className={classes.weekSelector}>
+                <img src={rightArrow} alt={"right-arrow"} />
+                {weeks[0].period}
+                <img src={leftArrow} alt={"left-arrow"} />
+            </div>
         </div>
     );
 }
