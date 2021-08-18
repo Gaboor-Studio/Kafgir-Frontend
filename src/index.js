b3ee9dff2f2626ac9c7a1ca804e76d5d2124cbd8
P_Moonesi <pmoonesi@aut.ac.ir>
2021-08-18 17:39:09 +0430

    trying to resolve conflicts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
+import './FontsFree-Net-ir_sans.ttf';
 
 ReactDOM.render(
   <BrowserRouter>
