44bc2e25b33602813e5f62559af3666ceca40486
KianoshArian <kianosharian3@gmail.com>
2021-08-31 12:25:10 +0430

    added profile picture

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Categories from "./Components/Pages/Categories/Categories";
 import Planner from "./Components/Pages/Planner/Planner";
 import Recipe from "./Components/Pages/Recipe/Recipe";
 import Home from "./Components/Pages/Home/Home";
+import Profile from "./Components/Pages/Profile/Profile"
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         </Route>
         <Route path="/recipe">
           <Recipe />
+        </Route>
+        <Route path="/profile">
+          <Profile />
         </Route>
         <Route path="/">
           <Home />
---
+++ src/Components/Pages/Profile/Profile.js
@@ -0,0 +1,24 @@
+import React from "react";
+import defaultProfilePic from "../../../assets/profile/default-profile-pic.jpg"
+import newPhoto from "../../../assets/profile/new_photo.png"
+import deletePhoto from "../../../assets/profile/delete_photo.png"
+import pictureBackground from "../../../assets/profile/picture-background.png"
+import classes from "./Profile.module.css";
+const Profile=()=>{
+    return(
+        <div>
+            <div className={classes.upperPart}>
+                <div className={classes.profilePicContainer}>
+                    <img src={pictureBackground} alt="profile avatar background" className={classes.profilePicBackground}></img>
+                    <img src={defaultProfilePic} alt="avatar" className={classes.profilePic}></img>
+                </div>
+                <div className={classes.editPhoto}>
+                    <img src={deletePhoto} alt="delete avatar"></img>
+                    <img src={newPhoto} alt="new avatar"></img>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
