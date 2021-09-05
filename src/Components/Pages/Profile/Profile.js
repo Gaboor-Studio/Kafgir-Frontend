f66a82a543e682d9e8806f9fd7ff58bfd3be28b8
KianoshArian <kianosharian3@gmail.com>
2021-09-05 18:02:29 +0430

    add the new changes to profile page

--- src/Components/Pages/Profile/Profile.js
+++ src/Components/Pages/Profile/Profile.js
@@ -5,13 +5,15 @@ import emailIcon from "../../../assets/profile/email.svg"
 import logoutIcon from "../../../assets/profile/logout.svg"
 import passwordIcon from "../../../assets/profile/password.svg"
 import contactUsIcon from "../../../assets/profile/contact-us.svg"
+import accountIcon from "../../../assets/profile/account.svg"
 const Profile = () => {
     return (
         <div>
             <UpperInformation></UpperInformation>
+            <ProfileCard title="نام کاربری" text="username" icon={accountIcon}></ProfileCard>
             <ProfileCard title="ایمیل" text="useremailexample@gmail.com" icon={emailIcon}></ProfileCard>
-            <ProfileCard title="تعویض رمز عبور" text="" icon={passwordIcon}></ProfileCard>
-            <ProfileCard title="تماس با ما" text="" icon={contactUsIcon}></ProfileCard>
+            <ProfileCard title="تعویض رمز عبور" text="" icon={passwordIcon} arrow></ProfileCard>
+            <ProfileCard title="تماس با ما" text="" icon={contactUsIcon} arrow></ProfileCard>
             <ProfileCard title="خروج از حساب کاربری" text="" icon={logoutIcon} isend></ProfileCard>
         </div>
     );
--- src/Components/Pages/Profile/ProfileCard.js
+++ src/Components/Pages/Profile/ProfileCard.js
@@ -9,9 +9,9 @@ const ProfileCard = (props) => {
                 <img src={props.icon} className={classes.emailIcon} alt="icon"></img>
                 <p className={classes.title}>{props.title}</p>
                 <p className={classes.cardText}>{props.text}</p>
-                <img src={leftArrow} className={classes.arrow} alt="arrow"></img>
+                <img src={leftArrow} className={props.arrow ? classes.arrow : classes.noArrow} alt="arrow"></img>
             </div>
-            <div className={props.isend ? [] : classes.border}></div>
+            <div className={props.isend ? [] :props.text!==""? classes.border : classes.borderTextless}></div>
         </div>
     );
 }
