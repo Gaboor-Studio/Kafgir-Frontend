fd7c9889c2bdbc1b24d5d977b168b57b77b71a3f
KianoshArian <kianosharian3@gmail.com>
2021-09-18 17:53:38 +0430

    improve admin panel layout

--- src/Components/Layout/AdminPanelLayout.js
+++ src/Components/Layout/AdminPanelLayout.js
@@ -3,26 +3,34 @@ import classes from "./AdminPanelLayout.module.css"
 import { Link } from 'react-router-dom'
 
 const AdminPanelLayout = (props) => {
-    const [currentPage, setCurrentPage] = useState('');
-
+    const [currentPage, setCurrentPage] = useState(window.location.pathname);
+    const [pageHeader, setPageHeader] = useState('');
+    useEffect(() => {
+        currentPage === "/admin-panel/food" ? setPageHeader("مدیریت غذا ها") : currentPage === "/admin-panel/tags-and-categories"?
+        setPageHeader("مدیریت تگ ها و کتگوری ها") : currentPage === "/admin-panel/user-management" ? setPageHeader("مدیریت کاربران") :
+        setPageHeader("مدیریت ادمین ها");
+    }, [currentPage])
     return (
         <div className={classes.pageContainer} dir="rtl">
             <div className={classes.layout}>
                 <p className={classes.title}>پنل ادمین</p>
                 <Link to="/admin-panel/food" className={classes.link}>
-                    <button className={currentPage === "food" ? [classes.button, classes.buttonSelected].join(' ') : classes.button} onClick={() => setCurrentPage('food')}>اطلاعات غذا</button>
+                    <button className={currentPage === "/admin-panel/food" ? [classes.button, classes.buttonSelected].join(' ') : classes.button} onClick={() => setCurrentPage('/admin-panel/food')}>اطلاعات غذا</button>
                 </Link>
                 <Link to="/admin-panel/tags-and-categories" className={classes.link}>
-                    <button className={currentPage === "categories" ? [classes.button, classes.buttonSelected].join(' ') : classes.button} onClick={() => setCurrentPage('categories')}>تگ ها و کتگوری ها</button>
+                    <button className={currentPage === "/admin-panel/tags-and-categories" ? [classes.button, classes.buttonSelected].join(' ') : classes.button} onClick={() => setCurrentPage('/admin-panel/tags-and-categories')}>تگ ها و کتگوری ها</button>
                 </Link>
                 <Link to="/admin-panel/user-management" className={classes.link}>
-                    <button className={currentPage === "users" ? [classes.button, classes.buttonSelected].join(' ') : classes.button} onClick={() => setCurrentPage('users')}>مدیریت کاربران</button>
+                    <button className={currentPage === "/admin-panel/user-management" ? [classes.button, classes.buttonSelected].join(' ') : classes.button} onClick={() => setCurrentPage('/admin-panel/user-management')}>مدیریت کاربران</button>
                 </Link>
                 <Link to="/admin-panel/admin-management" className={classes.link}>
-                    <button className={currentPage === "admins" ? [classes.button, classes.buttonSelected].join(' ') : classes.button} onClick={() => setCurrentPage('admins')}>مدیریت ادمین ها</button>
+                    <button className={currentPage === "/admin-panel/admin-management" ? [classes.button, classes.buttonSelected].join(' ') : classes.button} onClick={() => setCurrentPage('/admin-panel/admin-management')}>مدیریت ادمین ها</button>
                 </Link>
             </div>
-            {props.children}
+            <div className={classes.infoSection}>
+                <div className={classes.pageHeader}><p className={classes.header}>{pageHeader}</p></div>
+                {props.children}
+            </div>
         </div>
     );
 }
