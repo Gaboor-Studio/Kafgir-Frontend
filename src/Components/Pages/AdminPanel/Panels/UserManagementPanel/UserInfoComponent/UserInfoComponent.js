9afe4d5c980a2540d04c1c393ce75e8dfb4c4094
KianoshArian <kianosharian3@gmail.com>
2021-09-24 19:55:55 +0330

    fixed user management bug

--- src/Components/Pages/AdminPanel/Panels/UserManagementPanel/UserInfoComponent/UserInfoComponent.js
+++ src/Components/Pages/AdminPanel/Panels/UserManagementPanel/UserInfoComponent/UserInfoComponent.js
@@ -53,13 +53,12 @@ const UserInfoComponent = (props) => {
 
     const submitChanges = () => {
         setSubmitModal(false);
-        let reqData = {
-            "name": newName === '' ? props.name : newName,
-            "last_name": newLastName === '' ? props.lastName : newLastName,
-            "is_active": (!deactivateUser)
-        }
-        if (newUsername !== '')
-            reqData.username = newUsername;
+        let reqData={};
+        if (newUsername !== '')  reqData.username = newUsername;
+        if (newName !== '') reqData.name = newName;
+        if (newLastName !== '') reqData.last_name = newLastName;
+        reqData.is_active = (!deactivateUser);
+        console.log(reqData)
         axios.put([`http://84.241.22.193:8000/api/admin/user-management/${props.id}/`], reqData, config)
             .then(res => props.update())
             .catch(err => console.error(err));
@@ -81,7 +80,7 @@ const UserInfoComponent = (props) => {
         <div className={isExpanded ? [classes.container, classes.containerExpanded].join(' ') : classes.container}>
             <img src={props.profilePic} className={isExpanded ? classes.profilePic : classes.hide} />
             <p className={isExpanded ? editUsername ? classes.hide : classes.username : classes.lineViewText} onClick={() => setEditUsername(true)}>نام کاربری: {props.username}</p>
-            <p className={isExpanded ? editName ? classes.hide : classes.name : classes.lineViewText} onClick={() => setEditName(true)}>نام و نام خانوادگی: {[props.name,props.lastName].join(' ')}</p>
+            <p className={isExpanded ? editName ? classes.hide : classes.name : classes.lineViewText} onClick={() => setEditName(true)}>نام و نام خانوادگی: {[props.name, props.lastName].join(' ')}</p>
             <p className={isExpanded ? editEmail ? classes.hide : classes.email : classes.lineViewText} onClick={() => setEditEmail(true)}>ایمیل: {props.email}</p>
             <p className={isExpanded ? classes.access : classes.lineViewText}>نوع کاربر: {props.access}</p>
             <img src={arrow} className={isExpanded ? [classes.arrow, classes.upsidedown].join(' ') : classes.arrow} onClick={toggleExpand} />
